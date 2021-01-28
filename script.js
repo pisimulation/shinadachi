877c5fcef62158985eb15118e3f4327f0a0f138f
Shin Adachi <adachi.shin@gmail.com>
2021-01-28 18:17:11 -0500

    shuffle reviews

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('hello world');
+
+function shuffleReviews() {
+	var ul = document.getElementById('shuffle');
+	for (var i = ul.children.length; i >= 0; i--) {
+    	ul.appendChild(ul.children[Math.random() * i | 0]);
+	}
+}
